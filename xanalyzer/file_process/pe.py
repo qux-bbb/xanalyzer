ca07711183b4681e65767f4427731c4610be9e2b
qux-bbb <1147635419@qq.com>
2022-03-10 23:49:15 +0800

    pe模块获取数据整理到相应函数，便于测试

--- xanalyzer/file_process/pe.py
+++ xanalyzer/file_process/pe.py
@@ -60,22 +60,68 @@ class PeAnalyzer:
                     continue
 
         return versioninfo
-    
+
+    def get_compile_time(self):
+        time_str = datetime.fromtimestamp(self.pe_file.FILE_HEADER.TimeDateStamp)
+        return time_str
+
+    def get_pdb_path(self):
+        for debug_entry in getattr(self.pe_file, 'DIRECTORY_ENTRY_DEBUG', []):
+            if hasattr(debug_entry.entry, 'PdbFileName'):
+                return debug_entry.entry.PdbFileName.decode('utf8')
+        return
+
+    def get_peid_result(self):
+        signatures = peutils.SignatureDatabase(Config.peid_signature_path)
+        matches = signatures.match(self.pe_file, ep_only=True)
+        return matches
+
+    def verify_cert(self):
+        cert_info_list = []
+        security_index = pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_SECURITY"]
+        if len(self.pe_file.OPTIONAL_HEADER.DATA_DIRECTORY) <= security_index:
+            return
+        security_entry = self.pe_file.OPTIONAL_HEADER.DATA_DIRECTORY[security_index]
+        if not security_entry.Size or not security_entry.VirtualAddress:
+            return
+        with open(self.file_path, 'rb') as f:
+            try:
+                pe = SignedPEFile(f)
+                for signed_data in pe.signed_datas:
+                    cert = signed_data.certificates[0]
+                    cert_info = {}
+                    cert_info['subject'] = cert.subject.dn
+                    cert_info['issuer'] = cert.issuer.dn
+                    cert_info['serial_number'] = cert.serial_number
+                    cert_info['valid_from'] = cert.valid_from
+                    cert_info['valid_to'] = cert.valid_to
+
+                    try:
+                        signed_data.verify()
+                        cert_info['verify_result'] = 'valid'
+                    except Exception as e:
+                        cert_info['verify_result'] = 'invalid: {}'.format(e)
+                    cert_info_list.append(cert_info)
+            except Exception as e:
+                log.error('Error while parsing:')
+                log.error('{}'.format(e))
+        return cert_info_list
+
     def compile_time_scan(self):
         """
-        获取编译时间
+        查看编译时间
         """
-        time_str = datetime.fromtimestamp(self.pe_file.FILE_HEADER.TimeDateStamp)
-        log.info('compile time: {}'.format(time_str))
+        time_str = self.get_compile_time()
+        if time_str:
+            log.info('compile time: {}'.format(time_str))
 
     def pdb_scan(self):
         """
         查看pdb路径
         """
-        for debug_entry in getattr(self.pe_file, 'DIRECTORY_ENTRY_DEBUG', []):
-            if hasattr(debug_entry.entry, 'PdbFileName'):
-                log.info('pdb path: {}'.format(debug_entry.entry.PdbFileName.decode('utf8')))
-                return
+        pdb_path = self.get_pdb_path()
+        if pdb_path:
+            log.info('pdb path: {}'.format(pdb_path))
 
     def versioninfo_scan(self):
         """
@@ -91,42 +137,28 @@ class PeAnalyzer:
         """
         查壳
         """
-        signatures = peutils.SignatureDatabase(Config.peid_signature_path)
-        matches = signatures.match(self.pe_file, ep_only=True)
+        matches = self.get_peid_result()
         if matches:
-            log.info(matches)
+            log.info('packer: {}'.format(matches))
 
     def cert_scan(self):
         """
         输出证书信息并验证
         """
-        security_index = pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_SECURITY"]
-        if len(self.pe_file.OPTIONAL_HEADER.DATA_DIRECTORY) <= security_index:
-            return
-        security_entry = self.pe_file.OPTIONAL_HEADER.DATA_DIRECTORY[security_index]
-        if not security_entry.Size or not security_entry.VirtualAddress:
-            return
-        with open(self.file_path, 'rb') as f:
-            try:
-                pe = SignedPEFile(f)
-                for signed_data in pe.signed_datas:
-                    cert = signed_data.certificates[0]
-                    log.info('Contains certificates:')
-                    log.info('   Subject: {}'.format(cert.subject.dn))
-                    log.info('   Issuer: {}'.format(cert.issuer.dn))
-                    log.info('   Serial: {}'.format(cert.serial_number))
-                    log.info('   Valid from: {}'.format(cert.valid_from))
-                    log.info('   Valid to: {}'.format(cert.valid_to))
-
-                    try:
-                        signed_data.verify()
-                        log.info('Signature: valid')
-                    except Exception as e:
-                        log.warning('Signature: invalid')
-                        log.warning('{}'.format(e))
-            except Exception as e:
-                log.error('Error while parsing:')
-                log.error('{}'.format(e))
+        cert_info_list = self.verify_cert()
+        if cert_info_list:
+            log.info('contains certificates:')
+            for cert_info in cert_info_list:
+                log.info('   Subject: {}'.format(cert_info.get('subject', '')))
+                log.info('   Issuer: {}'.format(cert_info.get('issuer', '')))
+                log.info('   Serial number: {}'.format(cert_info.get('serial_number', '')))
+                log.info('   Valid from: {}'.format(cert_info.get('valid_from', '')))
+                log.info('   Valid to: {}'.format(cert_info.get('valid_to', '')))
+                verify_result = cert_info.get('verify_result', '')
+                if verify_result == 'valid':
+                    log.info('   Verify result: {}'.format(verify_result))
+                else:
+                    log.warning('   Verify result: {}'.format(verify_result))
 
     def run(self):
         self.compile_time_scan()
