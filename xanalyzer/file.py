c41a320ea8f159b0d743ae780e819c15f821d71d
qux-bbb <1147635419@qq.com>
2022-03-13 15:47:25 +0800

    文件字符串提取

--- xanalyzer/file.py
+++ xanalyzer/file.py
@@ -1,9 +1,13 @@
 # coding:utf8
 
+import os
+import re
 import magic
+from pathlib import Path
 
 from xanalyzer.utils import log
 from xanalyzer.file_process.pe import PeAnalyzer
+from xanalyzer.config import Config
 
 
 class FileAnalyzer():
@@ -16,13 +20,33 @@ class FileAnalyzer():
     
     def get_type(self):
         return magic.from_file(self.file_path)
-    
+
+    def get_strs(self):
+        the_file = open(self.file_path, 'rb')
+        file_content = the_file.read()
+        the_file.close()
+        all_strs = re.findall(rb'[\x21-\x7e]{4,}', file_content)
+        return all_strs
+
+    def str_scan(self):
+        all_strs = self.get_strs()
+        if all_strs:
+            log.info(f'str num: {len(all_strs)}')
+            if Config.conf['save_flag']:
+                str_file_name = Path(self.file_path).name+'_strings.txt'
+                str_data_path = os.path.join(Config.conf['analyze_data_path'], str_file_name)
+                with open(str_data_path, 'wb') as f:
+                    for a_str in all_strs:
+                        f.write(a_str+b'\n')
+                log.info(f'{str_file_name} saved')
+
     def search_str(self, want='ctf'):
         # TODO 查找敏感字符串
         pass
 
     def run(self):
         log.info('file type: {}'.format(self.file_type))
+        self.str_scan()
         if self.file_type.startswith('PE'):
             pe_analyzer = PeAnalyzer(self.file_path)
             pe_analyzer.run()
