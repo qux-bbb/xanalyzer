3c37fa6e8b117465975417db1c43583379ddd937
qux-bbb <1147635419@qq.com>
2022-03-20 12:39:48 +0800

    包含tools_info.json,调整版本号

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='xanalyzer',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     package_data={
-        'xanalyzer': ['data/UserDB.TXT'],
+        'xanalyzer': [
+            'data/UserDB.TXT',
+            'data/tools_info.json'
+        ],
     },
     author='qux-bbb',
     description='Analyzer for files and urls',
