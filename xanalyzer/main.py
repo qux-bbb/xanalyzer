933e78e0fa8b98bc42ded82f6473e4cd5e93622d
qux-bbb <1147635419@qq.com>
2022-03-26 22:04:15 +0800

    增加文件夹处理

--- xanalyzer/main.py
+++ xanalyzer/main.py
@@ -3,6 +3,7 @@
 
 import os
 import argparse
+from pathlib import Path
 
 from xanalyzer.file import FileAnalyzer
 from xanalyzer.url import UrlAnalyzer
@@ -15,27 +16,42 @@ def main():
         prog='xanalyzer',
         description='Process some files and urls.')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--file', nargs='+', help='analyze one or more files')
+    group.add_argument('-f', '--file', nargs='+', help='analyze one or more files, can be a folder path')
     group.add_argument('-u', '--url', help='analyze the url')
     parser.add_argument('-s', '--save', action='store_true', help='save log and data')
     args = parser.parse_args()
 
     Config.init(args.save)
     init_log()
+
+    log.info('=' * 80)
     
     if args.file:
-        for file_path in args.file:
-            if os.path.exists(file_path):
-                log.info('processing {}'.format(file_path))
-                file_analyzer = FileAnalyzer(file_path)
-                file_analyzer.run()
+        file_paths = []
+        for the_path in args.file:
+            if os.path.exists(the_path):
+                if os.path.isdir(the_path):
+                    for a_path in Path(the_path).iterdir():
+                        if a_path.is_file():
+                            file_paths.append(str(a_path))
+                        else:
+                            log.warning(f'{a_path} is not a file, will be ignored')
+                else:
+                    file_paths.append(the_path)
             else:
-                log.warning('{} does not exist!!!'.format(file_path))
+                log.warning('{} does not exist!!!'.format(the_path))
+        for file_path in file_paths:
+            log.info('processing {}'.format(file_path))
+            file_analyzer = FileAnalyzer(file_path)
+            file_analyzer.run()
+            log.info('-' * 80)
     if args.url:
+
         log.info('processing {}'.format(args.url))
         url_analyzer = UrlAnalyzer(args.url)
         url_analyzer.run()
         # TODO 增加url的处理方式, 集成 WebSiteLinkScanner 和 PageFinder
+        log.info('-' * 80)
 
     if Config.conf['save_flag']:
         log.info('the log and data are saved to {} folder'.format(Config.conf['analyze_path']))
