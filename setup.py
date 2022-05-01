fba0f406ee2f16ad3af940a8405b5c82045b6dc4
qux-bbb <1147635419@qq.com>
2022-05-01 13:01:27 +0800

    增加CHANGELOG，修改版本

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xanalyzer',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     package_data={
         'xanalyzer': [
